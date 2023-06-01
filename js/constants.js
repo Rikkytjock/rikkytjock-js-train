d4f29073367c0e6f7511155d9bf2dbdcdf6fbac1
Rikard <mohlin.rikard@gmail.com>
2023-06-01 11:32:02 +0200

    added info on constants

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,9 @@
 
+const interestRate = 0.3;
+interestRate = 1; 
+console.log(interestRate);
 
 
 
 // Use const when a variable should never be changed. Use let for everything else. 
+// We cannot re-asign a constant 
