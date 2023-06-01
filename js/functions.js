48e3f9edaba71f1d0986ba0901fbbd1429221333
Rikard <mohlin.rikard@gmail.com>
2023-06-01 15:34:58 +0200

    added of types-of-functions

---
+++ js/functions.js
@@ -0,0 +1,11 @@
+
+function greet(name, lastName) { // pirameter of the greet funtion
+    console.log('Hello' + name + '' + lastName);
+}
+
+greet('John', 'Smith'); //John is an argument to the greet function
+
+
+// inside curly bracers, body of the function
+// When we declare a function we dont have to end with ; 
+// Default value of variables is undefined
---
+++ js/types-of-functions.js
@@ -0,0 +1,14 @@
+
+// Peforming a tast
+function greet(name, lastName) {
+    console.log('Hello' + name + '' + lastName);
+}
+
+// Calculating a value
+function square (number) {
+    return number * number;
+}
+
+console.log(square(2));
+
+// Types of functions are bascily a set of statements that either peforms a task or calculates a value.
