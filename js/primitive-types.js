c67fd673592f25fd25a97212203f4533ebfc7bc6
Rikard <mohlin.rikard@gmail.com>
2023-06-01 11:38:50 +0200

    added info on primitive-types

---
+++ js/primitive-types.js
@@ -0,0 +1,11 @@
+
+let name = 'Rikard'; //String Literal
+let age = 28; // Number Literal
+let isApproved = true; // Boolean Literal (Can only be true & false)
+let firstName = undefined; // it has not been asigned a value, nor has it been decleared. 
+let selectedColor = null; // Use when we want to clear the value of a variable. 
+
+
+// Primitive/Value Types = String, number bollean, undefined, null 
+
+// Undefined
