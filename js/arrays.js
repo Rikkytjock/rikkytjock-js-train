216f1e9c0890e77461a1b1ee2d4c032969b311a4
Rikard <mohlin.rikard@gmail.com>
2023-06-01 15:20:23 +0200

    added info in arrays

---
+++ js/arrays.js
@@ -0,0 +1,11 @@
+
+let selectedColors = ['red', 'blue'];
+selectedColors[2] = '1';
+console.log(selectedColors.length);
+
+
+
+// Arrays are used to story list
+// [] = Array literal 
+// 'Arrays is a data structure the represent a list of items'
+// Arrays start from 0
---
+++ js/objects.js
@@ -0,0 +1,19 @@
+
+let person = {
+    name: 'Rikard',
+    age: 30
+};
+
+
+// Dot Notation
+person.name = 'John';
+
+console.log(person.name);
+
+// Bracket Notaion
+let selection = 'name';
+person[selection] = 'Mary';
+
+// Reference Types: Object, Array, Function
+// {} = Object literal 
+// Dot notation should be defualt choice. 
